fccc545dc0389c7b5303c7a375e4f24e4186f44a
ItzAxxel <helloimaxeluwu@gmail.com>
2021-04-29 10:50:05 +0200

    Finalización del juego

--- pong.py
+++ pong.py
@@ -7,6 +7,11 @@ wn.bgcolor("Black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
+
+#Marcador
+marcadorA = 0
+marcadorB = 0
+
 #JugadorA
 jugadorA = turtle.Turtle()
 jugadorA.speed(0)
@@ -27,13 +32,13 @@ jugadorB.shapesize(stretch_wid=5, stretch_len=  1)
 
 #Pelota 
 pelota = turtle.Turtle()
-pelota.speed(-1)
+pelota.speed(0)
 pelota.shape("circle")
 pelota.color("white")
 pelota.penup()
 pelota.goto(0,0)
-pelota.dx = 3
-pelota.dy = 3
+pelota.dx = 0.5
+pelota.dy = 0.5
 
 
 #Linea Division
@@ -41,6 +46,15 @@ division = turtle.Turtle()
 division.color("white")
 division.goto(0,400)
 division.goto(0,-400)
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("JugadorA: 0          JugadorB: 0", align= "center", font=("Courier", 24, "normal"))
 
 #Funciones
 def jugadorA_up():
@@ -91,8 +105,23 @@ while True:
     if pelota.xcor() > 390:
         pelota.goto(0,0)
         pelota.dx *= -1
+        marcadorA += 1
+        pen.clear()
+        pen.write("JugadorA: {}          JugadorB:{}".format(marcadorA,marcadorB), align= "center", font=("Courier", 24, "normal"))
+
     if pelota.xcor() < -390:
         pelota.goto(0,0)
         pelota.dx *= -1
+        marcadorB += 1
+        pen.clear()
+        pen.write("JugadorA: {}          JugadorB: {}".format(marcadorA,marcadorB), align= "center", font=("Courier", 24, "normal"))
 
-    if (pelota.xcor() > 340 and pelota.xcor() < 350)
+    if ((pelota.xcor() > 340 and pelota.xcor() < 350)
+        and (pelota.ycor() < jugadorB.ycor() + 50
+        and pelota.ycor() > jugadorB.ycor() -50)):
+            pelota.dx *= -1
+
+    if ((pelota.xcor() < -340 and pelota.xcor() > -350)
+        and (pelota.ycor() < jugadorA.ycor() + 50
+        and pelota.ycor() > jugadorA.ycor() -50)):
+            pelota.dx *= -1
